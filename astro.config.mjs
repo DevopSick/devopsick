c5ee2849181b76af92feaa06cbed08dffd54d9ab
DevopSick <122615635+DevopSick@users.noreply.github.com>
2025-08-31 10:33:02 +0000

    add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
