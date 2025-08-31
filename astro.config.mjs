2d97a1e3dbb055252bb1340c5b6f7edd24553a9b
DevopSick <122615635+DevopSick@users.noreply.github.com>
2025-08-31 10:17:44 +0000

    +

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,21 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
+        },
+    },
     integrations: [mdx(), sitemap(), react()],
 });
